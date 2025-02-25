ab8b72b874255a94023e938d52a4e401c0582423
psmith <peytonsmith2022@gmail.com>
2025-02-25 12:05:47 -0800

    deleted .Ruserdata

--- scripts/git_practice.R
+++
@@ -1,4 +0,0 @@
-usethis::use_git_config(user.name="psmith", user.email="peytonsmith2022@gmail.com", pull.rebase="false")
-usethis::git_default_branch_configure(name = "main")
-usethis::git_sitrep()
-
